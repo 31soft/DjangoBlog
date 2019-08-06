78c598e3bb16ea20c93a563c25930607bb366bf1
31soft <avtomatsoft@mail.ru>
2019-08-06 17:53:18 +0300

    Deploy

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -29,7 +29,7 @@ DISQUS_WEBSITE_SHORTNAME = '***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,6 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update()
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
